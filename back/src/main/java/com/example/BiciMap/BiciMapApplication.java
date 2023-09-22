243560398ff32c93dc93e72eb9c1a772f213888b
kelly.miranda <kelly.miranda@constructorabolivar.com>
2023-09-22 12:17:35 -0500

    back

---
+++ back/src/main/java/com/example/BiciMap/BiciMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.BiciMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiciMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiciMapApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/BiciMap/BiciMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BiciMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiciMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
